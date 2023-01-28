18cc0fd81af9b0d3fe81cacac1c6bb4fd9a54d49
toa.pita <toa.pita@gmail.com>
2023-01-28 15:52:22 -0700

    Add project files.

---
+++ mission04_toapita/wwwroot/js/main.js
@@ -0,0 +1,54 @@
+﻿$("#button").click(function () {
+    // when the button gets clicked go through all the textboxes and convert the score to float
+    let assignmentGrade = parseFloat($("#assignments").val())
+    let groupProjectGrade = parseFloat($("#groupproject").val())
+    let quizzesGrade = parseFloat($("#quizzes").val())
+    let midtermGrade = parseFloat($("#midtermexam").val())
+    let finalExamGrade = parseFloat($("#finalexam").val())
+    let intexGrade = parseFloat($("#intex").val())
+
+    // calculate the final grade
+    let finalGrade = (assignmentGrade * .50) + (groupProjectGrade * .1) + (quizzesGrade * .1)
+        + (midtermGrade * .1) + (finalExamGrade * .1) + (intexGrade * .1)
+
+    // calculate and output letter grade
+    $("#result").html(calcLetterGrade(finalGrade))
+
+})
+
+function calcLetterGrade(finalGrade) {
+
+    //convert to decimal
+    finalGrade = finalGrade / 100;
+
+    // calculate the letter grade based on the scale in learningsuite
+    if (finalGrade >= .94) {
+        letterGrade = "A";
+    } else if (finalGrade >= .90) {
+        letterGrade = "A-";
+    } else if (finalGrade >= .87) {
+        letterGrade = "B+";
+    } else if (finalGrade >= .84) {
+        letterGrade = "B";
+    } else if (finalGrade >= .80) {
+        letterGrade = "B-";
+    } else if (finalGrade >= .77) {
+        letterGrade = "C+";
+    } else if (finalGrade >= .74) {
+        letterGrade = "C";
+    } else if (finalGrade >= .70) {
+        letterGrade = "C-";
+    } else if (finalGrade >= .67) {
+        letterGrade = "D+";
+    } else if (finalGrade >= .64) {
+        letterGrade = "D";
+    } else if (finalGrade >= .60) {
+        letterGrade = "D-";
+    } else {
+        letterGrade = "E";
+    }
+
+    //return the last grade
+    return letterGrade;
+
+}
